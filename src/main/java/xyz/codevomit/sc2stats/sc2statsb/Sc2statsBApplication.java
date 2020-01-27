912e20893cf200ef3720c68d010ac2c6bb5f7f3c
Rospaccio <alberto.mercati@gmail.com>
2020-01-27 18:51:07 +0100

    Initial commit

---
+++ src/main/java/xyz/codevomit/sc2stats/sc2statsb/Sc2statsBApplication.java
@@ -0,0 +1,13 @@
+package xyz.codevomit.sc2stats.sc2statsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc2statsBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc2statsBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/codevomit/sc2stats/sc2statsb/Sc2statsBApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.codevomit.sc2stats.sc2statsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sc2statsBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
